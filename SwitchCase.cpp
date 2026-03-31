6dbf81765274a394f1f9dd5185b1645e58c117cf
rmezzeluna-png <rmezzeluna@gmail.com>
2026-03-31 19:07:16 +0700

    output

--- SwitchCase.cpp
+++ SwitchCase.cpp
@@ -35,3 +35,18 @@ void pilihanJurusan() {
             statusKuota = "Error: Kode Tidak Valid";
     }
 }
+
+void output() {
+    cout << "\n=== HASIL SELEKSI ===" << endl;
+    cout << "Jurusan Pilihan : " << namaJurusan << endl;
+    cout << "Status Kuota    : " << statusKuota << endl;
+    cout << "-----------------------------------" << endl;
+}
+
+int main() {
+    input();
+    pilihanJurusan();
+    output();
+
+    return 0;
+}
