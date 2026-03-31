6a0603c264ebecbb3ef7fbbf6650cb93db4bb6ac
rmezzeluna-png <rmezzeluna@gmail.com>
2026-03-31 19:05:23 +0700

    input

--- SwitchCase.cpp
+++ SwitchCase.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+// Deklarasi variable global
+int kodeJurusan;
+string namaJurusan;
+string statusKuota;
+
+void input() {
+    cout << "=== PENDAFTARAN JURUSAN KAMPUS ===" << endl;
+    cout << "1. Teknologi Informasi" << endl;
+    cout << "2. Teknik Elektro" << endl;
+    cout << "3. Sistem Informasi" << endl;
+    cout << "-----------------------------------" << endl;
+    cout << "Masukkan kode jurusan (1-3): ";
+    cin >> kodeJurusan;
+}
