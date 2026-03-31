9c98e807559630aa15ace1c3426bc61aed9702dc
rmezzeluna-png <rmezzeluna@gmail.com>
2026-03-31 16:26:10 +0700

    int

--- looping.cpp
+++ looping.cpp
@@ -1,3 +1,27 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
+
+int main(){
+
+int x;
+
+//Perulangan dengan FOR
+//mencetak tulisan seabnyak 5 kali
+cout<<"PERULANGAN FOR"<<endl;
+for(int i=0;i<=4;i++){
+    cout<<"Teknologi Informasi UMY"<<endl;
+}
+cout<<endl;
+//untuk mendapatkanb urutan angka acak yg berbeda
+srand(time(0));
+
+//Perulangan dengan WHILE
+cout<<"PERULANGAN WHILE"<<endl;
+x = 1 + rand()%10;
+
+while(x<=5){
+    cout<<"Bilangan acak = "<<x <<endl;
+    x = rand()%10;
+}
+
