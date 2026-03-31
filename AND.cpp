45b3b5cac75f611d5b5f85da5897c94be3fc3c40
rmezzeluna-png <rmezzeluna@gmail.com>
2026-03-31 19:01:29 +0700

    endl

--- AND.cpp
+++ AND.cpp
@@ -9,3 +9,13 @@ cout<<"Masukkan nilai Matematika = ";
 cin>>nilM;
 cout<<"Masukkan nilai Bahasa Inggris = ";
 cin>>nilB;
+
+rerata = (nilB+nilM)/2;
+
+if ((rerata >= 60) && (nilM >= 70))
+    status = "Lulus";
+else
+    status = "Tidak lulus";
+
+cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
+}
